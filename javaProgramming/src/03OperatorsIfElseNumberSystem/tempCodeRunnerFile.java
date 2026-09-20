32a0ee115fdf3121f84fb7a39de265ac069405df
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2026-09-20 21:12:43 +0530

    Add Car and Driver classes with basic functionality for driving and fuel management

--- javaProgramming/src/03OperatorsIfElseNumberSystem/tempCodeRunnerFile.java
+++
@@ -1,1 +0,0 @@
-areaOfTriangle
---
+++ javaProgramming/src/06ClassesAndObjects/Driver.java
@@ -0,0 +1,10 @@
+
+
+public class Driver {
+    
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.drive();
+        System.out.println(myCar.currentFuelInLiters);
+    }
+}
